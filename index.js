b019982b4f45639b49ecc5ec986f1aca26fb865a
Luis Westberry <takuame7@gmail.com>
2023-07-08 00:50:57 -0500

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const eventName = ["suprise"];
+function writeCards(cards, event) {
+    let messagesArray = [];
+    for (let i = 0; i < cards.length; i++) {
+      messagesArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return messagesArray;
+  }
+
+  function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+  }
